c5ba99839926542a3dfa0dfc646bdc35bbfa575c
Jeferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2020-09-12 13:02:06 +0100

    add a sample test

---
+++ test/hooks/postorgcreate/installPackages.test.ts
@@ -0,0 +1,9 @@
+import assert = require('assert');
+
+describe('postorgcreate hook install package', () => {
+  // throw new Error('');
+  //assert.fail('Here');
+  it('should pass', function () {
+    assert.ok(true);
+  });
+});
