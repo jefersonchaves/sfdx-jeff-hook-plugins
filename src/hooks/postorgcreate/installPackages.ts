f19227375233e3be94ceb7dd5901907cfc712927
Jeferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2020-09-13 10:26:01 +0100

    First implementation of package installation

--- src/hooks/postorgcreate/installPackages.ts
+++ src/hooks/postorgcreate/installPackages.ts
@@ -1,4 +1,8 @@
 import { Command, Hook } from '@oclif/config';
+import { SfdxProject } from '@salesforce/core';
+import cli from 'cli-ux';
+import * as fs from 'fs-extra';
+import * as util from 'util';
 
 // tslint:disable-next-line: no-any
 type HookFunction = (this: Hook.Context, options: HookOptions) => any;
@@ -24,11 +28,37 @@ type HookOptions = {
 };
 
 export const hook: HookFunction = async options => {
+  const exec = util.promisify(require('child_process').exec);
+
   console.log('PostOrgCreate Hook Running');
 
   if (options.result) {
-    console.log('Username created: ' + options.result.username);
+    cli.action.start('Installing packages');
+
+    const project = await readSfdxProject();
+
+    if (project.packageAliases) {
+      for (const packageAlias of Object.keys(project.packageAliases)) {
+        console.log('Installing ' + packageAlias);
+        // TODO: retrieve installation key, maybe config to allow specialized retrieval?
+        // TODO: error handling
+        // const { stdout, stderr } = await exec('sfdx force:package:install ... --json');
+        await exec('sfdx force:package:install --package="' + packageAlias + '" --wait=10 --json');
+        console.log(packageAlias + ' Installed');
+      }
+    } else {
+      // TODO: should it display any message?
+    }
+    cli.action.stop();
   }
 };
 
 export default hook;
+
+async function readSfdxProject() {
+  const sfdxProject = await SfdxProject.resolve();
+  const isGlobalProject = false;
+  const projectFile = await sfdxProject.retrieveSfdxProjectJson(isGlobalProject);
+  const project = await fs.readJson(projectFile.getPath());
+  return project;
+}
