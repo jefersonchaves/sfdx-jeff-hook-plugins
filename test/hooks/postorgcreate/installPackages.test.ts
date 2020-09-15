ce1fbed76cc6e8690a089801f8971dafc5026d44
Jeferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2020-09-15 18:57:24 +0100

    Clear install package test

--- test/hooks/postorgcreate/installPackages.test.ts
+++ test/hooks/postorgcreate/installPackages.test.ts
@@ -1,8 +1,6 @@
 import * as assert from 'assert';
 
 describe('postorgcreate hook install', () => {
-  // throw new Error('');
-  //assert.fail('Here');
   it('should pass', function () {
     assert.ok(true);
   });
